a50be896d88a24e9fdb4d6074347f12ef0acff53
h0n3yBadg3r <614512@gmail.com>
2015-03-02 19:05:58 -0800

    Yammer Authentication Sample in Swift

---
+++ Yammer_Authentication_Swift/Yammer Authentication Swift/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Yammer Authentication Swift
+//
+//  Created by SwiftBlog on 3/2/15.
+//  Copyright (c) 2015 SwiftBlog. All rights reserved.
+//
+
+
+import Cocoa
+
+
+class ViewController: NSViewController {
+
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+    }
+
+
+    override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+    }
+
+
+
+
+}
