07bfd4d66359bc0b38df5f3059b0b0c21d00c684
h0n3yBadg3r <614512@gmail.com>
2015-03-04 16:07:49 -0800

    Salesforce Authentication Sample App Swift

---
+++ Salesforce_Authentication_Swift/Salesforce Authentication SwiftTests/Salesforce_Authentication_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Salesforce_Authentication_SwiftTests.swift
+//  Salesforce Authentication SwiftTests
+//
+//  Created by SwiftBlog on 3/4/15.
+//  Copyright (c) 2015 SwiftBlog. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Salesforce_Authentication_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
